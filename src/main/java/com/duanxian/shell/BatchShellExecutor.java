b1e2bf692990b8a20c1c33751e4b601df7ab58bf
yanbizha <yanbing.zhang@nokia-sbell.com>
2017-09-01 18:23:06 +0800

    Small updates.

--- src/main/java/com/duanxian/shell/BatchShellExecutor.java
+++ src/main/java/com/duanxian/shell/BatchShellExecutor.java
@@ -12,17 +12,19 @@ import java.util.concurrent.*;
 public class BatchShellExecutor
 {
     private static final int POOL_SIZE = 20;
-    private static final int TIMEOUT = 60;
+    private static final int TIMEOUT = 120;
     private static final Logger LOGGER = LogManager.getLogger(BatchShellExecutor.class);
+    private static final ExitCode EXIT_CODE = new ExitCode();
 
     public void executeCommands(List<String> commandList)
     {
         long startTime = System.currentTimeMillis();
         ExecutorService executorService = Executors.newFixedThreadPool(POOL_SIZE);
-        CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(executorService);
+        CompletionService<Integer> completionService = new ExecutorCompletionService<>(executorService);
         for (String command : commandList)
         {
             long threadStartTime = System.currentTimeMillis();
+            LOGGER.debug("Starting new thread for command execution: "+command);
             ShellExecutorThread executorThread = new ShellExecutorThread(command);
             completionService.submit(executorThread);
             Future<Integer> future;
@@ -31,6 +33,7 @@ public class BatchShellExecutor
                 future = completionService.take();
                 Integer exitCode = future.get(TIMEOUT, TimeUnit.SECONDS);
                 LOGGER.info("Exit code is: " + exitCode);
+                LOGGER.info("Command execution result is: " + EXIT_CODE.getResultByExitCode(exitCode));
                 long timeCost = System.currentTimeMillis() - threadStartTime;
                 LOGGER.info("Time cost: " + timeCost + " ms.");
             }
@@ -47,7 +50,9 @@ public class BatchShellExecutor
                 LOGGER.error("Shell execution failed due to timeout.");
             }
         }
+        executorService.shutdown();
         long totalTime = System.currentTimeMillis() - startTime;
         LOGGER.info("Total time cost is: " + totalTime);
+        System.exit(0);
     }
 }
